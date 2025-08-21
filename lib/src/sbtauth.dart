39fa18e328e373cbe2650ff8d72392baee6f288c
orange <1486996133@qq.com>
2025-08-21 21:23:08 +0800

    备份公钥

--- lib/src/sbtauth.dart
+++ lib/src/sbtauth.dart
@@ -669,11 +669,13 @@ class SbtAuth {
     }
     // 解密
     final restoredData = await restorePublicKeyInfo(encryptedFragment.first.publicKeyBackUpInfo, '111111');
+    print('sbt-120');
     print(restoredData);
 
     for (String key in restoredData.keys) {
       String? privateKey;
       SbtChain chain = SbtChain.EVM;
+      print(key);
       switch (key) {
         case 'evm':
           privateKey = restoredData['evm'];
